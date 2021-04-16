351a77cd0762fdf1e140c531bb07938425440032
zh-hang <zhangh127@163.com>
2021-04-16 15:26:55 +0800

    Fixed some problems with view render

--- suplerlists/lists/tests.py
+++ suplerlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -10,16 +10,17 @@ class HomePageTest(TestCase):
 
     def test_users_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'homt.html')
+        self.assertTemplateUsed(response, 'wrong.html')
+        
 
     # def test_root_url_resolve_to_home_page_view(self):
     #     found = resolve('/')  # 解析url的function
     #     self.assertEqual(found.func, home_page)
 
     # def test_home_page_returns_correst_html(self):
-    #     # request = HttpRequest()
-    #     # response = home_page(request)
-    #     response=self.client.get('/')
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     # response=self.client.get('/')
     #     html = response.content.decode('utf-8')
     #     # expected_html=render_to_string('home.html')
     #     # self.assertEqual(html,expected_html)
@@ -27,7 +28,7 @@ class HomePageTest(TestCase):
     #     self.assertIn('<title>To-Do lists</title>', html)
     #     self.assertTrue(html.endswith('</html>'))
 
-    #     self.assertTemplateUsed(response,'homt.html')
+        # self.assertTemplateUsed(response,'homt.html')
 # 用于验证该测试机制是否运行
 # class SomkeTest(TestCase):
 
