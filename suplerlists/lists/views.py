6a413a79b878813da5a8dbe928878e642ce86df4
zh-hang <zhangh127@163.com>
2021-04-23 22:17:33 +0800

    修改了view的input输入和test的输入测试

--- suplerlists/lists/views.py
+++ suplerlists/lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request,'home.html')
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
 # Create your views here.
