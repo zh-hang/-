4202f5bc3bed6450f2476e1e5d030f3a6fd67913
zh-hang <zhangh127@163.com>
2021-05-07 15:43:38 +0800

    fix bug in functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -18,10 +18,10 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text,[row.text for row in rows])
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
@@ -33,30 +33,45 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_css_selector('h1').text
         self.assertIn('To-Do', header_text)
 
+        inputbox=self.browser.find_element_by_id('id_new_item')
         # 检测有没有一个输入todo的输入框
-        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
+
+        inputbox=self.browser.find_element_by_id('id_new_item')
         # 向输入框中输入Buy peacock feathers
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # 检验是否插入
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
+        # 检验是否插入     
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feather'for row in rows),
         #     F'New to-do item did not appear in table. Contents were:\n{table.text}'
-        # )
+        # )  
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
 
-        inputbox.send_keys('Use peacock feathers to make a fly')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # 无论如何都会产生错误信息
