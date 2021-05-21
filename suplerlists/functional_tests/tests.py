638a76b7ffc150ab5235ed7eebc5c4de1e99ab9a
zh-hang <zhangh127@163.com>
2021-05-21 16:02:14 +0800

    use WebDriverException

--- suplerlists/functional_tests/tests.py
+++ suplerlists/functional_tests/tests.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 import unittest
 import time
 
+MAX_WAIT=10
 
-# 新的测试类，继承unittest.TestCase
+# 新的测试类，继承LiveServerTestCase
 # 以test开头的都是测试方法A
 
 
@@ -19,10 +21,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self,row_text):
+        start_time=time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError,WebDriverException)as e:
+                if time.time()-start_time>MAX_WAIT:
+                    raise e
+                time.sleep
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
@@ -34,46 +44,28 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_css_selector('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox=self.browser.find_element_by_id('id_new_item')
         # 检测有没有一个输入todo的输入框
+        inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-
-        inputbox=self.browser.find_element_by_id('id_new_item')
         # 向输入框中输入Buy peacock feathers
+        inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # 检验是否插入
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
+        # 向输入框中输入Use peacock feathers to make a fly
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 检验是否插入     
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feather'for row in rows),
-        #     F'New to-do item did not appear in table. Contents were:\n{table.text}'
-        # )  
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # 无论如何都会产生错误信息
         self.fail("Finish the test!")
